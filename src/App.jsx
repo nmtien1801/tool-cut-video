c71ffb7798fa1c2d790dbc4be4a8dec88fd70380
nmtien1801 <nickname1801@gmail.com>
2026-04-21 08:39:11 +0700

    fix crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 import Login from './page/Login';
